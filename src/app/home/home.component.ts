153e35c890320f9ae754fd30e65d59850fa4005a
LamPhong <you@example.comlamphong9559@gmail.com>
2021-08-19 21:44:48 +0700

    combobox quận huyện

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,47 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Phong';
   public age = 20;
+   public TRAICAY = ['Trái cây 1', 'Trái cây 2'];
+   public TRAICAY2 = [
+                      {ten: 'Trái cây 1', dongia:10, hagia:true}
+                      , {ten: 'Trái cây 3', dongia:20, hagia:false}
+                      , {ten: 'Trái cây 5', dongia:40, hagia:true}
+                      , {ten: 'Trái cây 8', dongia:60, hagia:false}
+                    ];
+
+  
+  public DanhSachThanhPho = [
+    {
+      MaThanhPho:1
+      , TenThanhPho:'Hồ Chí Minh'
+      , DanhSachQuan:[{
+                        MaQuan:'1'
+                        , TenQuan:'Quận 1'
+                      }
+                      , {
+                        MaQuan:'2'
+                        , TenQuan:'Quận 2'
+                      }
+                    ]
+    }
+    , {
+      MaThanhPho:2
+      , TenThanhPho:'Hà Nội'
+      , DanhSachQuan:[{
+                        MaQuan:'11'
+                        , TenQuan:'Quận Hoàn Kiếm'
+                      }
+                      , {
+                        MaQuan:'12'
+                        , TenQuan:'Quận Cầu Giấy'
+                      }
+                    ]
+    }                    
+  
+  ]
+
+  public DanhSachQuan: any[] = []
+   
 
   constructor() { 
     
@@ -21,4 +62,17 @@ export class HomeComponent implements OnInit {
     console.log('ResetName');
     this.name = '';
   }
+
+
+  onOptionsSelected(value:any){
+    console.log("the selected value is " + value);
+    var MaThanhPhoChon: number = +value;
+    const searchThanhPho = this.DanhSachThanhPho.filter(x => x.MaThanhPho === MaThanhPhoChon);
+    
+    this.DanhSachQuan = searchThanhPho[0].DanhSachQuan;
+  }
+
+
 }
+
+
